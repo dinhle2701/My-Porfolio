63ba7141935ecfa365029017fee10287f27ce987
dinh-letat <dinhprococoa@gmail.com>
2025-05-08 23:29:37 +0700

    fix: change Skill

--- src/components/Experience/Badge/Badge.jsx
+++ src/components/Experience/Badge/Badge.jsx
@@ -3,15 +3,16 @@ import Image from 'next/image'
 import badges from '@/data/badge'
 
 const Badge = () => {
-    
+
     return (
         <div className='badge text-black'>
             <p>This is a Badge</p>
-            {badges.map((index) => {
+            {badges.map((badge, index) => {
                 <div key={index}>
                     <Image className='me-2 transition-transform duration-700 ease-in-out group-hover:rotate-[360deg]'
-                        src={badges.image}
-                        alt="TailwindCSS"
+                        src={badge.image}
+                        alt={`Badge ${index + 1}`}
+                        fill
                         width={80}
                         height={80} />
                 </div>
